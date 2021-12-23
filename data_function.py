7127979159263c9bcaa77d6f061895e25eccacb9
Ellis <elliszkn@163.com>
2021-12-23 17:21:43 +0800

    fix bug in datafunc

--- data_function.py
+++ data_function.py
@@ -49,28 +49,30 @@ class ImageData(torch.utils.data.Dataset):
 
 class GTResDataset(torch.utils.data.Dataset):
 
-	def __init__(self, root_path, gt_dir=None, transform=None, transform_train=None):
-		self.pairs = []
-		for f in os.listdir(root_path):
-			image_path = os.path.join(root_path, f)
-			gt_path = os.path.join(gt_dir, f)
-			if f.endswith(".jpg") or f.endswith(".png"):
-				self.pairs.append([image_path, gt_path.replace('.png', '.jpg'), None])
-		self.transform = transform
-		self.transform_train = transform_train
+    def __init__(self, root_path, gt_dir=None, transform=None, transform_train=None):
+        self.pairs = []
+        for f in os.listdir(root_path):
+            f_ = f.replace('predict','origin')
+            image_path = os.path.join(root_path, f)
+            gt_path = os.path.join(gt_dir, f_)
+            # if f.endswith(".jpg") or f.endswith(".png"):
+            #     self.pairs.append([image_path, gt_path.replace('.png', '.jpg'), None])
+            self.pairs.append([image_path, gt_path, None])
+        self.transform = transform
+        self.transform_train = transform_train
 
-	def __len__(self):
-		return len(self.pairs)
+    def __len__(self):
+        return len(self.pairs)
 
-	def __getitem__(self, index):
-		from_path, to_path, _ = self.pairs[index]
-		from_im = Image.open(from_path).convert('RGB')
-		to_im = Image.open(to_path).convert('RGB')
+    def __getitem__(self, index):
+        from_path, to_path, _ = self.pairs[index]
+        from_im = Image.open(from_path).convert('RGB')
+        to_im = Image.open(to_path).convert('RGB')
 
-		if self.transform:
-			to_im = self.transform(to_im)
-			from_im = self.transform(from_im)
+        if self.transform:
+            to_im = self.transform(to_im)
+            from_im = self.transform(from_im)
 
-		return from_im, to_im
+        return from_im, to_im
 
         
